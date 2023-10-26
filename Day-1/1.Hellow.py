73ad57691c6bb89cf20ae7af5bfbe6102d2b233b
Shashikant Yadav <103162888+shashi310@users.noreply.github.com>
2023-10-26 12:42:40 +0530

    day-1-learing-python

---
+++ Day-1/1.Hellow.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+int=10
+
+char="my"
+
+print("{char} age is ${int}") #it will not print the variables
+
+print(f"{char} age is ${int}") #now it will
+
+#f"...": This denotes an f-string. An f-string is a formatted string literal, which allows you to embed expressions inside string literals, using curly braces {}. The expressions inside the curly braces are evaluated at runtime and then formatted and inserted into the string.
---
+++ Day-1/2.DataTypes.py
@@ -0,0 +1,36 @@
+#int float str bool list tuple dict set 
+
+
+# Integer
+integer_variable = 10
+
+# Float
+float_variable = 3.14
+
+# String
+string_variable = "Hello, world!"
+
+# Boolean
+boolean_variable = True
+
+# List
+list_variable = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_variable = (6, 7, 8, 9, 10)
+
+# Dictionary
+dictionary_variable = {'a': 1, 'b': 2, 'c': 3}
+
+# Set
+set_variable = {1, 2, 3, 4, 5}
+
+# Printing the types and values
+print(f"Type of integer_variable: {type(integer_variable)}, value: {integer_variable}")
+print(f"Type of float_variable: {type(float_variable)}, value: {float_variable}")
+print(f"Type of string_variable: {type(string_variable)}, value: {string_variable}")
+print(f"Type of boolean_variable: {type(boolean_variable)}, value: {boolean_variable}")
+print(f"Type of list_variable: {type(list_variable)}, value: {list_variable}")
+print(f"Type of tuple_variable: {type(tuple_variable)}, value: {tuple_variable}")
+print(f"Type of dictionary_variable: {type(dictionary_variable)}, value: {dictionary_variable}")
+print(f"Type of set_variable: {type(set_variable)}, value: {set_variable}")
